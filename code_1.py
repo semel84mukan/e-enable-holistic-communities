553ba704fa0a66c1da0680d5ba261ec92196fea2
semel84mukan <152295085+semel84mukan@users.noreply.github.com>
2023-12-10 10:04:10 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    for item in data:
+    return data
+def generate_random_data():
+if __name__ == "__main__":
+    main()
+
